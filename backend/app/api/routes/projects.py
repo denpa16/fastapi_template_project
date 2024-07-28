12c4aeb573835a5d6065c045732eb6d0a8bbc2a8
d.pavlov <d.pavlov@idaproject.com>
2024-07-28 09:53:14 +0300

    feat(main): add new filterset class

--- backend/app/api/routes/projects.py
+++ backend/app/api/routes/projects.py
@@ -1,8 +1,8 @@
 from random import random, randrange
 from uuid import UUID
 
-from fastapi import APIRouter
-from fastapi import Path
+from fastapi import APIRouter, Request
+from fastapi import Path, Depends
 from pydantic import Field
 from sqlalchemy import delete, exc, insert, select, update, bindparam
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -52,6 +52,119 @@ async def get_multiple(
 ):
     query = filter.filter(select(Project))
     result = await session.execute(query)
+    projects = result.scalars().all()
+    return projects
+
+
+class BaseFilter:
+    def __init__(
+        self,
+        field_name: str | None = None,
+        lookup_expr: str | None = None,
+    ):
+        if lookup_expr is None:
+            lookup_expr = "__eq__"
+        self.field_name = field_name
+        self.lookup_expr = lookup_expr
+
+
+class BaseInFilter:
+    def __init__(
+        self,
+        field_name: str | None = None,
+        lookup_expr: str | None = None,
+    ):
+        if lookup_expr is None:
+            lookup_expr = "in_"
+        self.field_name = field_name
+        self.lookup_expr = lookup_expr
+
+
+class BaseRangeFilter:
+    """TODO: _max и _min надо сделать."""
+
+    def __init__(
+        self,
+        field_name: str | None = None,
+        lookup_expr: str | None = None,
+    ):
+        if lookup_expr is None:
+            lookup_expr = "range_"
+        self.field_name = field_name
+        self.lookup_expr = lookup_expr
+
+
+class IntegerFilter(BaseFilter): ...
+
+
+class IntegerInFilter(BaseFilter): ...
+
+
+class RangeFilter(BaseRangeFilter): ...
+
+
+class CharFilter(BaseFilter): ...
+
+
+class CharInFilter(BaseInFilter): ...
+
+
+class FilterSet:
+    def __filter(self):
+        query = select(self.Meta.model)
+        _filters = self.__get_filters()
+        for key, value in self.__query_params:
+            if _filter := _filters.get(key):
+                try:
+                    model_field = getattr(self.Meta.model, key)
+                except AttributeError:
+                    continue
+                if isinstance(_filter, BaseFilter):
+                    query = query.filter(
+                        getattr(model_field, _filter.lookup_expr)(value)
+                    )
+                if isinstance(_filter, BaseInFilter):
+                    query = query.filter(
+                        getattr(model_field, _filter.lookup_expr)(value.split(","))
+                    )
+        return query
+
+    @classmethod
+    def __filter_list(cls):
+        return cls.__dict__.keys()
+
+    def __get_filters(self):
+        filters = {}
+        for key in self.__filter_list():
+            attr = getattr(self, key)
+            if isinstance(attr, BaseFilter | BaseInFilter):
+                filters[key] = attr
+        return filters
+
+    def __call__(self, request, *args, **kwargs):
+        self.__query_params = request.query_params.items()
+        return self.__filter()
+
+    class Meta:
+        model = None
+
+
+class TProjectFilter(FilterSet):
+    name = CharInFilter()
+    alias = CharInFilter()
+
+    class Meta:
+        model = Project
+
+
+@router.get("/test")
+async def get_test_multiple(
+    _: Request,
+    project_filter: TProjectFilter = Depends(TProjectFilter),
+    session: AsyncSession = async_session,
+):
+    sm = project_filter(_)
+    result = await session.execute(sm)
     projects = result.scalars().all()
     return projects
 
